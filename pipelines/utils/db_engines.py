bebf687379324b776de970dd0d7a07909da7f808
Kelvin Kimutai <kelvin.kimutai@savannahinformatics.com>
2022-05-07 17:37:41 +0300

    Fix mch_user DAG

--- pipelines/utils/db_engines.py
+++ pipelines/utils/db_engines.py
@@ -4,12 +4,14 @@ from sqlalchemy.engine import create_engine
 
 # Retrieve URIs from Secret Manager
 secret_manager_client = secretmanager.SecretManagerServiceClient()
-
 ssl_mode = "?sslmode=require"
-ssl_args = {"ssl_ca": "/opt/mycarehub/plugins/certs/server-ca.pem",
-            "ssl_cert": "/opt/mycarehub/plugins/certs/client-cert.pem",
-            "ssl_key": "/opt/mycarehub/plugins/certs/client-key.pem"}
-
+ssl_args = {
+    "ssl": {
+        "ssl_ca": "/opt/mycarehub/plugins/certs/server-ca.pem",
+        "ssl_cert": "/opt/mycarehub/plugins/certs/client-cert.pem",
+        "ssl_key": "/opt/mycarehub/plugins/certs/client-key.pem"
+    }
+}
 mch_cloudsql_uri = secret_manager_client.access_secret_version(
     "projects/sghi-307909/secrets/mch_cloudsql_uri/versions/1"
 ).payload.data.decode("utf-8")
