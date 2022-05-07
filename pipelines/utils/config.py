135a08cdc26d8d2285fb703b0464b54ae93d7082
Kelvin Kimutai <kelvin.kimutai@savannahinformatics.com>
2022-05-07 15:36:11 +0300

    Fix mch_user DAG

--- pipelines/utils/config.py
+++ pipelines/utils/config.py
@@ -3,7 +3,7 @@ from pipelines.utils import google_clients
 
 # get & deserialize config.toml from gcs
 c_client = google_clients.initialize_gcs_client()
-c_bucket = c_client.get_bucket("mycarehub-elt-queries")
+c_bucket = c_client.get_bucket("mycarehub-queries")
 c_blob = c_bucket.get_blob("config/config.toml")
 toml_string = c_blob.download_as_string().decode("utf-8")
 parsed_config = toml.loads(toml_string)
