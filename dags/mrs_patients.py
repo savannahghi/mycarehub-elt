5141571529b470e80e4d8de3b00b2e982b82a05f
Kelvin Kimutai <kelvin.kimutai@savannahinformatics.com>
2022-05-09 10:35:17 +0300

    Add KenyaEMR patients DAG

---
+++ dags/mrs_patients.py
@@ -0,0 +1,49 @@
+"""Airflow DAG to run ETL Jobs."""
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from pipelines.extract import trigger
+
+from pipelines.utils import config, db_engines
+
+# Initialize DAG
+default_args = {
+    "owner": "Open MRS",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 10, 12),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 4,
+    "retry_delay": timedelta(minutes=10),
+}
+
+etl_dag = DAG(
+    dag_id="mrs_patients",
+    description="Patients on OpenMRS",
+    schedule_interval="0 */4 * * *",
+    default_args=default_args,
+    catchup=False,
+)
+
+patients_ext = PythonOperator(
+    task_id="patients_extract_to_gcs",
+    python_callable=trigger.trigger_to_gcs,
+    op_kwargs={'folder': config.emr_patients_fold,
+               'engine': db_engines.openmrs_engine,
+               'bucket': config.emr_patients_bket},
+    dag=etl_dag
+)
+
+patients_load = PythonOperator(
+    task_id="patients_load_to_bquery",
+    python_callable=trigger.trigger_to_bquery,
+    op_kwargs={'folder': config.emr_patients_fold,
+               'dataset': config.emr_patients_dset,
+               'bucket': config.emr_patients_bket},
+    dag=etl_dag
+)
+
+# Task Dependencies
+patients_ext >> patients_load
--- pipelines/utils/db_engines.py
+++ pipelines/utils/db_engines.py
@@ -8,9 +8,14 @@ secret_manager_client = secretmanager.SecretManagerServiceClient()
 mch_cloudsql_uri = secret_manager_client.access_secret_version(
     "projects/sghi-307909/secrets/mch_cloudsql_uri/versions/1"
 ).payload.data.decode("utf-8")
-
+openmrs_cloudsql_uri = secret_manager_client.access_secret_version(
+    "projects/sghi-307909/secrets/openmrs_cloudsql_uri/versions/1"
+).payload.data.decode("utf-8")
 
 # Generate DB engines
 mycarehub_engine = create_engine(
     mch_cloudsql_uri,
+)
+openmrs_engine = create_engine(
+    openmrs_cloudsql_uri,
 )
