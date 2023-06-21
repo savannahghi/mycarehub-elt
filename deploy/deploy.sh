d371de46cc9e4bc8937fc893725273c006274f94
Salaton <nairouasalaton@gmail.com>
2023-06-21 12:36:42 +0300

    feat(ci): implement github actions for CI/CD workflows (#3)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,25 +6,34 @@ set -eux
 kubectl create namespace $NAMESPACE || true
 
 # Delete Kubernetes secret if exists
-kubectl delete secret matrix-service-account --namespace $NAMESPACE || true
+kubectl delete secret airflow-service-account airflow-ssh-git-secret airflow-postgresql --namespace $NAMESPACE || true
 
 # Create GCP service account file
 cat $GOOGLE_APPLICATION_CREDENTIALS >> ./service-account.json
 
+kubectl create secret generic airflow-ssh-git-secret --namespace $NAMESPACE --from-literal=id_rsa="$AIRFLOW_SSH_SECRET"
+
+kubectl create secret generic airflow-postgresql --namespace $NAMESPACE --from-literal=postgresql-password="$POSTGRES_PASSWORD"
+
 # Recreate service account file as Kubernetes secret
-kubectl create secret generic matrix-service-account \
+kubectl create secret generic airflow-service-account \
     --namespace $NAMESPACE \
     --from-file=key.json=./service-account.json
+
+helm repo add airflow-stable https://airflow-helm.github.io/charts
+
+helm repo update
 
 helm upgrade \
     --install \
     --debug \
     --create-namespace \
     --namespace "${NAMESPACE}" \
-    --set externalDatabase.user="${POSTGRES_USER}"\
-    --set externalDatabase.password="${POSTGRES_PASSWORD}"\
     --wait \
-    --timeout 300s \
-    -f ./charts/matrix/values.yaml \
+    --version "8.7.1" \
+    --timeout 900s \
+    -f ./charts/airflow/values.yaml \
     $APP_NAME \
-    ./charts/matrix
+    airflow-stable/airflow
+
+# kubectl create secret generic airflow-ssh-git-secret --namespace airflow --from-file=id_rsa=$HOME/.ssh/id_rsa
