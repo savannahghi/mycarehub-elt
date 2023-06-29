9b6da6bf63647e7afa24d37938fb7a2a3d4187ba
Salaton <nairouasalaton@gmail.com>
2023-06-29 15:18:47 +0300

    chore: add a test dag for QA (#15)

---
+++ dags/test_dag.py
@@ -0,0 +1,55 @@
+"""Airflow DAG to run ETL Jobs."""
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from pipelines.extract import trigger
+from pipelines.staging import run
+from pipelines.utils import config, db_engines
+
+# Initialize DAG
+default_args = {
+    "owner": "mycarehub",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 10, 12),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 4,
+    "retry_delay": timedelta(minutes=10),
+}
+
+etl_dag = DAG(
+    dag_id="test_dag",
+    description="Registered users on Mycarehub",
+    schedule_interval="0 */4 * * *",
+    default_args=default_args,
+    catchup=False,
+)
+
+users_ext = PythonOperator(
+    task_id="users_extract_to_gcs",
+    python_callable=trigger.trigger_to_gcs,
+    op_kwargs={'folder': config.mch_users_fold,
+               'engine': db_engines.mycarehub_engine,
+               'bucket': config.mch_users_bket},
+    dag=etl_dag
+)
+
+users_load = PythonOperator(
+    task_id="users_load_to_bquery",
+    python_callable=trigger.trigger_to_bquery,
+    op_kwargs={'folder': config.mch_users_fold,
+               'dataset': config.mch_users_dset,
+               'bucket': config.mch_users_bket},
+    dag=etl_dag
+)
+patients_staging = PythonOperator(
+    task_id="mch_patients_staging",
+    python_callable=run.trigger_staging,
+    op_kwargs={'folder': config.mch_users_staging_fold,
+               'dataset': config.mch_users_dset},
+    dag=etl_dag
+)
+# Task Dependencies
+users_ext >> users_load >> patients_staging
